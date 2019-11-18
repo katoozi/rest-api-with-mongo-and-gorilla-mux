73415115e6623aa0788b25b8b711f82d68f1c7ee
mohammad katoozi <k2527806@gmail.com>
2019-11-18 12:31:09 +0330

    create constantd.go and move person to constants.go

--- main.go
+++ main.go
@@ -18,16 +18,6 @@ import (
 	"go.mongodb.org/mongo-driver/x/bsonx"
 )
 
-// Person is the data structure that we will save and receive.
-type Person struct {
-	ID        primitive.ObjectID     `json:"_id,omitempty" bson:"_id,omitempty"`
-	FirstName string                 `json:"first_name,omitempty" bson:"first_name,omitempty"`
-	LastName  string                 `json:"last_name,omitempty" bson:"last_name,omitempty"`
-	Username  string                 `json:"username,omitempty" bson:"username,omitempty"`
-	Email     string                 `json:"email,omitempty" bson:"email,omitempty"`
-	Data      map[string]interface{} `json:"data,omitempty" bson:"data,omitempty"`
-}
-
 var people *mongo.Collection
 
 // results count per page
