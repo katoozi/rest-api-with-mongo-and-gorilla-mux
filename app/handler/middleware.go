b31f3d2544da3098a0c28fce49ae9225ce39cd05
mohammad katoozi <k2527806@gmail.com>
2019-11-28 11:53:48 +0330

    add charset to content-type header field

--- app/handler/middleware.go
+++ app/handler/middleware.go
@@ -5,7 +5,7 @@ import "net/http"
 // JSONContentTypeMiddleware will add the json content type header for all endpoints
 func JSONContentTypeMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("content-type", "application/json")
+		w.Header().Add("content-type", "application/json; charset=UTF-8")
 		next.ServeHTTP(w, r)
 	})
 }
